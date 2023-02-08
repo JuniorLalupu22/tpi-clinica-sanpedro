e8b2615fd7bef405015e3bf56da53a88951d2988
JuniorLalupu22 <jlalupui@unprg.edu.pe>
2023-02-08 11:29:16 -0500

    actualizar backend

--- src/keys/backend_keys.js
+++ src/keys/backend_keys.js
@@ -1,4 +1,4 @@
-const url = "https://back-clinica-production.up.railway.app"
+const url = "https://back-clinica-production.up.railway.app/"
 // const url = "https://backend-tpi-production-570e.up.railway.app"
 
 export default url;

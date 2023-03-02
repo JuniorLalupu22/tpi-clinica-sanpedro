7f801c8d0eaef5912990576a2323cd249248443a
JuniorLalupu22 <jlalupui@unprg.edu.pe>
2023-03-01 22:27:48 -0500

    nueva actualizacion

--- src/keys/backend_keys.js
+++ src/keys/backend_keys.js
@@ -1,5 +1,5 @@
 // const url = "https://back-clinica-production.up.railway.app"
-const url = "http://localhost:5000"
-// const url = "https://back-clinica-production-a4be.up.railway.app"
+// const url = "http://localhost:5000"
+const url = "https://back-clinica-production-df8d.up.railway.app"
 
 export default url;
